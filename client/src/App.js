eea2f7355fc76a27d7dfb675e5b4a4734421ba6d
ccarraher <connercarraher@gmail.com>
2021-07-18 20:26:02 -0500

    How many times

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,7 @@ function App() {
       { urls: "stun:stun4.l.google.com:19302" }
     ]
   }
-  const SERVER_URL = "http://localhost:7000";
+  const SERVER_URL = "https://fierce-plains-31659.herokuapp.com/";
   useEffect(() => {
     socket.current = io.connect(SERVER_URL);
 
