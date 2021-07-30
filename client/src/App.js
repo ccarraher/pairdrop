0a64f5bd1406c58717d551a872934b3be153af36
ccarraher <connercarraher@gmail.com>
2021-07-30 09:29:41 -0500

    TURN is slow

--- client/src/App.js
+++ client/src/App.js
@@ -43,10 +43,6 @@ function App() {
       { urls: "stun:stun.l.google.com:19302" },
       { urls: "stun:stun1.l.google.com:19302" },
       { urls: "stun:stun2.l.google.com:19302" },
-      { urls: "turn:192.158.29.39:3478?transport=tcp",
-        credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-        username: "28224511:1379330808"
-      }
     ]
   }
   const SERVER_URL = "https://pairdrop.xyz";
